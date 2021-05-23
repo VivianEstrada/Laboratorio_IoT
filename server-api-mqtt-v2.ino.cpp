c70ab1d0f2595bca29135967836a4ebf06d98173
varelagrajales <82426999+varelagrajales@users.noreply.github.com>
2021-05-22 22:34:15 -0500

    Primera parte del lab III

--- server-api-mqtt-v2.ino.cpp
+++ server-api-mqtt-v2.ino.cpp
@@ -18,24 +18,24 @@ const char *root_topic_publish_humedad = "t3GQncnL0ObphtZ/humedad";
 const char *root_topic_subscribe_puerta = "t3GQncnL0ObphtZ/puerta";
 
 
-//**************************************
-//*********** WIFICONFIG ***************
-//**************************************
+//**************
+//**** WIFICONFIG ******
+//**************
 const char* ssid = "Varelas grajales";
 const char* password = "F4m1l14V4r3l4";
 const char* host = "maker.ifttt.com";
 
-//*************************************
-//************CONFIG EMAIL*************
-//*************************************
+//*************
+//*****CONFIG EMAIL****
+//*************
 String apiKey = "cIHL21wmXS7-MsB2fXc-t-"; //"YOUR_IFTTT_API_KEY";
 String eventName = "send_email";
 String value1, value2, value3;
 String request;
 
-//**************************************
-//*********** GLOBALES   ***************
-//**************************************
+//**************
+//**** GLOBALES   ******
+//**************
 WiFiClient espClient;
 PubSubClient client(espClient);
 const int DHTPin = 5;
@@ -45,9 +45,9 @@ DHT dht(DHTPin, DHTTYPE);
 Servo servo1;
 
 
-//**************************************
-//*********** VARIABLES  ***************
-//**************************************
+//**************
+//**** VARIABLES  ******
+//**************
 float t, h = 0;
 float lastT, lastH = 0;
 float tempLimit = 30.0;
@@ -88,4 +88,65 @@ void setup() {
   servo1.write(0);
   dht.begin();
   myTestCall();
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  if (client.connected()) {
+    lastT = dht.readTemperature();
+    h = dht.readHumidity();
+    if (!isnan(lastT)) {
+      t = lastT;
+    }
+    
+    strpublicar = "Temperatura: " + String(t) + "°C  " + "Humedad: " + String(h) + "%" + "Temperatura limite: " + tempLimit + " Estado de puerta: " + stateServo;
+    strpublicar.toCharArray(msg, 85);
+    client.publish(root_topic_publish_general, msg);
+    delay(200);
+
+    unsigned long topLoop1 = millis();
+    if (topLoop1 - lastPubleshTemp >= publeshTemp) {
+      lastPubleshTemp = topLoop1;
+      strtemp = "Temperatura: " + String(t) + "°C";
+      strtemp.toCharArray(msgtemp, 30);
+      client.publish(root_topic_publish_temperatura, msgtemp);
+      delay(200);
+    }
+    unsigned long topLoop2 = millis();
+    if (topLoop2 - lastPubleshHum >= publeshHum) {
+      lastPubleshHum = topLoop2;
+      strhum = "Humedad: " + String(h) + "%";
+      strhum.toCharArray(msghum, 30);
+      client.publish(root_topic_publish_humedad, msghum);
+      delay(200);
+    }
+    delay(3000);
+  }
+  client.loop();
+ //refri();
+  leerSubscripcion();
+  
+}
+
+void setup_wifi() {
+  delay(10);
+  // Nos conectamos a nuestra red Wifi
+  Serial.println();
+  Serial.print("Conectando a ssid: ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Conectado a red WiFi!");
+  Serial.println("Dirección IP: ");
+  Serial.println(WiFi.localIP());
 }
